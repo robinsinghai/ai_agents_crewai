15a72674e5bf4b25d5c1d733f1b780726e2af543
Robin <robin.rs1414@gmail.com>
2025-05-26 22:10:06 +0530

    youtube_

--- blog_writing/agents.py
+++ blog_writing/agents.py
@@ -1,16 +1,18 @@
 from crewai import Agent
 from tools import yt_tool
+import os
+
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_MODEL_NAME'] = "gpt-4o"
 
 ## Create a  senior blog content researcher
 
 blog_reseracher = Agent(
     role="Blog reseracher from youtube videos",
-    goal=" get the relevant video content from the topic {topic} from yt channel;",
+    goal=" get the relevant video content from the topic {topic} from yt channel",
     verbose=True,
     memory=True,
-    backstory=(
-        "Expert in understanding videos in AI Data Science, Machine Learning and Gen AI and providing suggestion",
-    ),
+    backstory="Expert in understanding videos in AI Data Science, Machine Learning and Gen AI and providing suggestion",
     tools=[yt_tool],
     allow_delegation=True
 )
@@ -23,11 +25,9 @@ blog_writer = Agent(
      goal = 'Narrate compelling tech stories about the video {topic} from yt channel',
      verbose=True,
      memory=True,
-     backstory=(
-         "With a flair for simplifying complex topics, you craft"
+     backstory="""With a flair for simplifying complex topics, you craft"
          "engaging narratives that captivate and educate, bringing new"
-         "discoveries to ligth in an accessible manner."
-     ),
+         "discoveries to ligth in an accessible manner.""",
      tools=[yt_tool],
      allow_delegation=False
 )
